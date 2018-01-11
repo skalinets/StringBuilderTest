bf795c8cdbb0239d774a6bc54b105b5bc99f91b0
Serhiy Kalinets <kalinets@gmail.com>
2018-01-11 17:46:10 +0200

    removes demo code

--- Program.cs
+++ Program.cs
@@ -8,26 +8,6 @@ using BenchmarkDotNet.Running;
 
 namespace StringBuilderTest
 {
-  public class Md5VsSha256
-  {
-    private const int N = 10000;
-    private readonly byte[] data;
-
-    private readonly SHA256 sha256 = SHA256.Create();
-    private readonly MD5 md5 = MD5.Create();
-
-    public Md5VsSha256()
-    {
-      data = new byte[N];
-      new Random(42).NextBytes(data);
-    }
-
-    [Benchmark]
-    public byte[] Sha256() => sha256.ComputeHash(data);
-
-    [Benchmark]
-    public byte[] Md5() => md5.ComputeHash(data);
-  }
 
   public class StringBuilderBenchmarks
   {
@@ -44,7 +24,6 @@ namespace StringBuilderTest
     public string StringBuilderAction() 
     {
         var sb = new StringBuilder();
-
         GetItems(NumberOfItems).ForEach(_ => sb.Append(_));
         return sb.ToString();
     }
